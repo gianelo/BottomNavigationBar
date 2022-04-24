941bd251eaad4430c6e7026d0b7a208663c194a8
gianelo <gianbarboza11@gmail.com>
2022-04-24 18:23:19 -0500

    Initial Commit

---
+++ BottomNavigationBar/BottomNavigationBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BottomNavigationBarApp.swift
+//  BottomNavigationBar
+//
+//  Created by Gian Barboza on 24/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BottomNavigationBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BottomNavigationBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BottomNavigationBar
+//
+//  Created by Gian Barboza on 24/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
